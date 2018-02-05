49a6fa75898066db5e6a4a5e81e787cd18d1a2bb
Shahilsky <shahilsky@gmail.com>
2018-02-05 22:53:17 +0530

    added template

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+        author = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True)
+        title = models.CharField(max_length=30, null=True)
+        text = models.TextField(null=True)
+        created_date = models.DateTimeField(default=timezone.now)
+        published_date = models.DateTimeField(null=True, blank=True)
+
+        def publish(self):
+            self.published_date = timezone.now()
+            self.save()
+
+        def __str__(self):
+            return self.title
--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
-# Create your views here.
+def home(request):
+    posts = Post.objects.all().order_by('published_date')
+    return render(request, 'Blog/home.html', {'posts':posts})
